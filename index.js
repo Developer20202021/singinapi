861a6db5c76c20a91d91afe3d14271f3538e11a4
MahadiHasan4281 <mahadikaushik8888@gmail.com>
2021-12-20 21:26:12 +0600

    done

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 
-const PORT = process.env.PORT||5000;
+let PORT = process.env.PORT||5000;
 
 
 const privateKey = process.env.Private_Key;
